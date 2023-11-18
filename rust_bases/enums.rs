2357cc933446503a3d3de6ea9849e0afeaddecd5
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-11-17 22:29:01 -0300

    Bases - If let

--- rust_bases/enums.rs
+++ rust_bases/enums.rs
@@ -10,7 +10,7 @@ enum Mes {
     Septiembre,
     Octubre,
     Noviembre,
-    Disiembre,
+    Diciembre,
 }
 
 enum Tiempo {
@@ -22,13 +22,17 @@ enum Tiempo {
 
 fn main() {
     let mi_tiempo = Tiempo::Dia(Mes::Julio);
+    let mi_tiempo_2 = Tiempo::Minutos;
 
-    let segundos = timpo_a_segundos(mi_tiempo);
+    let segundos = timpo_a_segundos(&mi_tiempo);
+    let contador = prueba_if_let(&mi_tiempo_2);
 
     println!("Son {segundos}s");
+
+    println!("{contador}");
 }
 
-fn timpo_a_segundos(t: Tiempo) -> u32 {
+fn timpo_a_segundos(t: &Tiempo) -> u32 {
     match t {
         Tiempo::Segundos => 1,
         Tiempo::Minutos => 60,
@@ -38,4 +42,16 @@ fn timpo_a_segundos(t: Tiempo) -> u32 {
             _ => 60 * 60 * 24,
         },
     }
+}
+
+fn prueba_if_let(t: &Tiempo) -> u32 {
+    let mut contador = 0_u32;
+
+    if let Tiempo::Minutos = t {
+        contador += 1;
+    } else {
+        println!("Tiempo distinto de minutos");
+    }
+
+    contador
 }
