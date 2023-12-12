ce3911227ec5f9100656ed2c288395d62d660eaf
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-12-12 19:11:18 -0300

    Archivos - Renombrando archivos

--- archivos/src/main.rs
+++ archivos/src/main.rs
@@ -1,8 +1,8 @@
 // Coloco el pub para que no me tire advertencias el compilador
-pub mod creando_json;
+pub mod deserealizar_json;
 pub mod leer_archivo;
 pub mod leer_csv;
-pub mod leer_json;
+pub mod serializando_json;
 
 fn main() {
     println!("Hola mundo");
