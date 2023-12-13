c8e68ce82407ed6b54465e40d684d33f57608dff
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-12-13 20:01:40 -0300

    HTTP - Post

--- http_request/src/main.rs
+++ http_request/src/main.rs
@@ -1,14 +1,57 @@
+use reqwest::blocking::Client;
+
 fn main() {
     get_users();
+    store_user();
 }
 
 fn get_users() {
     let url = "https://jsonplaceholder.typicode.com";
 
-    let body = reqwest::blocking::get(format!("{url}/users"))
-        .unwrap()
-        .text()
+    let client = Client::new();
+    let res = client.get(format!("{url}/users")).send().unwrap();
+
+    let body = res.text().unwrap();
+
+    println!("{body}");
+}
+
+fn store_user() {
+    let url = "https://jsonplaceholder.typicode.com";
+
+    let new_user = r#"{
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    }
+  "#;
+
+    let client = Client::new();
+    let res = client
+        .post(format!("{url}/users"))
+        .body(new_user)
+        .header("Content-type", "application/json; charset=UTF-8")
+        .send()
         .unwrap();
+
+    let body = res.text().unwrap();
 
     println!("{body}");
 }
